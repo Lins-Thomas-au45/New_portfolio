2b640ab068f0870ea736202a2e2cfd50adfcf62d
Lins Thomas <raphealins@gmail.com>
2022-09-22 00:29:20 +0530

    ready deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import React from 'react'
 import Header from './components/header/Header';
 import About from './components/about/About';
-import Nav from './components/nav/Nav'
+import Nav from './components/nav/Nav';
+import Contacts from './components/contacts/Contacts';
+import Portfolio from './components/portfolio/Portfolio';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer'
 
 
 function App() {
@@ -10,7 +14,11 @@ function App() {
     <>
     <Header/>
     <About/>
+    <Experience/>
     <Nav/>
+    <Portfolio/>
+    <Contacts/>
+    <Footer/>
     </>
   );
 }
