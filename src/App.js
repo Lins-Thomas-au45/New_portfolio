94e42a0bec03ca86c2ce3e2397b5ce3b12edd51b
Lins Thomas <raphealins@gmail.com>
2022-09-21 22:46:52 +0530

    header completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import React from 'react'
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div></div>
+    <>
+    <Header/>
+    </>
   );
 }
 
