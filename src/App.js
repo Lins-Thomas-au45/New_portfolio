14db35c632c0e4d7b4202ef22e06c616d54a1c4f
Lins Thomas <raphealins@gmail.com>
2022-09-21 23:34:37 +0530

    ready deploy

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import './App.css';
 import React from 'react'
 import Header from './components/header/Header';
 import About from './components/about/About';
-import Nav from './components/nav/Nav';
 import about from './components/about/About';
-import Footer from './components/footer/Footer'
 
 
 function App() {
   return (
     <>
     <Header/>
-      <Nav/>
-      <About/>
-      <Footer/>
+     <About/>
     </>
   );
 }
