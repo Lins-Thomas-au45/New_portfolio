174e3d28ff6fe6c2bc4f5871ce37784005b315bc
Lins Thomas <raphealins@gmail.com>
2022-09-21 23:38:22 +0530

    ready deploy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
 import React from 'react'
 import Header from './components/header/Header';
-import About from './components/about/About';
-import about from './components/about/About';
 
 
 function App() {
   return (
     <>
     <Header/>
-     <About/>
     </>
   );
 }
