08c35539e2f83e4fc4adcd04e42a7069c03c4a68
Lins Thomas <raphealins@gmail.com>
2022-10-02 20:23:57 +0530

    updated

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio6.PNG'
-import IMG2 from '../../assets/portfolio1.PNG'
-import IMG3 from '../../assets/portfolio2.PNG'
+import IMG2 from '../../assets/portfolio7.PNG'
+import IMG3 from '../../assets/netfilx_clone.PNG'
 import IMG4 from '../../assets/portfolio3.PNG'
 import IMG5 from '../../assets/portfolio4.PNG'
 import IMG6 from '../../assets/portfolio5.PNG'
@@ -20,7 +20,7 @@ const Portfolio = () => {
           <h3>My Portfolio</h3>
         <div className="portfolio__item-cta">
           <a href="https://github.com/Lins-Thomas-au45/New_portfolio.git" className='btn' target='_blank'>Github</a>
-          <a href="https://helpful-rugelach-ee2903.netlify.app" className='btn btn-primary' target= '_blank'>Live Demo</a>
+          <a href="lins-thomas-portfolio.netlify.app" className='btn btn-primary' target= '_blank'>Live Demo</a>
 
         </div>
         </article>
@@ -29,10 +29,10 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={IMG2} alt="" />
           </div>
-          <h3>still on going</h3>
+          <h3>Restaurant Listing wbsite</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/Lins-Thomas-au45/My_portfolio_website" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target= '_blank'>Live Demo</a>
+          <a href="https://github.com/Lins-Thomas-au45/Resturant_project.git" className='btn' target='_blank'>Github</a>
+          <a href="https://restaurant-listing-website.netlify.app" className='btn btn-primary' target= '_blank'>Live Demo</a>
           </div>
           </article>
 
@@ -40,10 +40,10 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={IMG3} alt="" />
           </div>
-          <h3>still ongoing</h3>
+          <h3>Netflix clone</h3>
           <div className="portfolio__item-cta">
-          <a href="http://github.com" className='btn'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target= '_blank'>Live Demo</a>
+          <a href="https://github.com/Lins-Thomas-au45/Netflix_clone_ui.git" className='btn'>Github</a>
+          <a href="https://netflixwebsitee.netlify.app" className='btn btn-primary' target= '_blank'>Live Demo</a>
           </div>
         </article>
 
